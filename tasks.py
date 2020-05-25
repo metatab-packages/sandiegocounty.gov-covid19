d80d8b256a9eb8fa1324d601773df14572c3fecc
Eric Busboom <eric@civicknowledge.com>
2020-05-25 15:37:31 -0700

    new tasks.py

--- tasks.py
+++ tasks.py
@@ -1,52 +1,37 @@
 # Task definitions for invoke
 # You must first install invoke, https://www.pyinvoke.org/
 
-from invoke import task
+from metapack_build.tasks.package import ns
 
-# Set this to the S3 bucket where you will publish public data packages
-# You must have a boto or .aws configuration and credentials to access the bucket
-# s3_bucket = 'library.metatab.org'
-s3_bucket = None
+# To configure options for invoke functions you can:
+# - Set values in the 'invoke' section of `~/.metapack.yaml
+# - Use one of the other invoke config options:
+#   http://docs.pyinvoke.org/en/stable/concepts/configuration.html#the-configuration-hierarchy
+# - Set the configuration in this file:
 
-# Set the wordpress site where package documentation and links will be published
-# The string is a reference to the `wordpress` section in  ~/.metapack.yaml .
-# wp_site = 'dataa.sandiegodata.org'
-wp_site = None
+#ns.configure(
+#    {
+#        'metapack':
+#            {
+#                's3_bucket': 'bucket_name',
+#                'wp_site': 'wp sot name',
+#                'groups': None,
+#                'tags': None
+#            }
+#    }
+# )
 
-# Groups will be come categories when publishing to Wordpress
-groups = ['Health']
-# Tags are tags when publilshing to wordpress.
-tags = ['county', 'national']
-
-group_flags = ' '.join([f"-g{g}" for g in groups])
-tag_flags = ' '.join([f"-t{t}" for t in tags])
-
-
-def force_flag(force):
-    return '-F' if force else ''
+# However, the `groups` and `tags` hould really be set in the `metatada.csv`
+# file, and `s3_bucket` and `wp_site` should be set at the collection or global level
 
 
-wp_flags = f' -w {wp_site} {group_flags} {tag_flags}' if wp_site else ''
-s3_flags = f' -s {s3_bucket}' if s3_bucket else ''
-
+# You can also create you own tasks
+from invoke import task, Collection
 
 @task
-def make(c, force=False):
-    """Build, write to S3, and publish to wordpress, but only if necessary"""
-    c.run(f'mp -q  make {force_flag(force)} -r  -b {s3_flags} {wp_flags}')
+def example_task(c):
+    """An exmaple Invoke task"""
+    c.run("echo 'this is an example task' ")
 
+ns.add_task(example_task)
 
-@task
-def build(c, force=False):
-    # Build a filesystem package.
-    c.run(f"mp build -r {force_flag(force)}")
-
-
-# Publish to s3 and wordpress, if the proper bucket and site
-# variables are defined
-@task
-def publish(c):
-    if s3_bucket:
-        c.run(f"mp s3 -s {s3_bucket}")
-    if wp_site:
-        c.run(f"mp wp -s {wp_site} {group_flags} {tag_flags} -p")
